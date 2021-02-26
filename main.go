a4918bb46128bcbab616949b30e6f5e3f4a9640c
Denis <d.khilkov@gmail.com>
2021-02-26 16:40:43 +0300

    seting server on port 8100

--- main.go
+++ main.go
@@ -1,28 +1,32 @@
 package main
 
 import (
+	"database/sql"
+	"log"
+
 	_ "github.com/jackc/pgx/v4/stdlib"
-	thirdparty "github.com/layouts/thirdParty"
+	"github.com/layouts/api"
+	db "github.com/layouts/db/sqlc"
 )
 
 const (
 	dbDriver     = "pgx"
 	dbSource     = "postgresql://root:WEBdeveloepr1452@localhost:5432/layouts?sslmode=disable"
-	serverAdress = "0.0.0.0:8080"
+	serverAdress = "0.0.0.0:8100"
 )
 
 func main() {
 	// thirdparty.GetLayouts()
-	thirdparty.AddPathAndCreateSvgData()
-	// conn, err := sql.Open(dbDriver, dbSource)
-	// if err != nil {
-	// 	log.Fatal("cannot connect to db: ", err)
-	// }
-	// store := db.NewStore(conn)
-	// server := api.NewServer(store)
+	// thirdparty.AddPathAndCreateSvgData()
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db: ", err)
+	}
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
 
-	// err = server.Start(serverAdress)
-	// if err != nil {
-	// 	log.Fatal("cannot start server: ", err)
-	// }
+	err = server.Start(serverAdress)
+	if err != nil {
+		log.Fatal("cannot start server: ", err)
+	}
 }
