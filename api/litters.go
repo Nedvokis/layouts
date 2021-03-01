d1200f19f07d16921ddea4c5f22990531acf7de5
Denis <d.khilkov@gmail.com>
2021-03-01 17:28:15 +0300

    refactoring letters api

--- api/litters.go
+++ api/litters.go
@@ -2,27 +2,30 @@ package api
 
 import (
 	"database/sql"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 type GetParentLittersRequest struct {
-	Parent int64 `form:"parent" binding:"required,min=1`
+	Parent int64 `form:"parent" binding:"min=1"`
 }
 
 func (server *Server) GetLittersList(ctx *gin.Context) {
 	var req GetParentLittersRequest
-	if err := ctx.BindQuery(&req); err == nil {
+	err := ctx.BindQuery(&req)
+	if err == nil {
 		litters, err := server.store.GetListLittersByParent(ctx, req.Parent)
 		if err != nil {
 			ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 			return
 		}
-
 		ctx.JSON(http.StatusOK, litters)
+		fmt.Println("I not supoused to be here: ", req.Parent)
 		return
 	}
+	fmt.Println("I was here")
 
 	litters, err := server.store.GetListAllLitters(ctx)
 	if err != nil {
