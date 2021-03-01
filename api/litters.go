a0a32b2d2962cefcbddfb90cc36c9b830d7e63d8
Denis <d.khilkov@gmail.com>
2021-03-01 16:06:49 +0300

    add to api litters parent query

--- api/litters.go
+++ api/litters.go
@@ -7,7 +7,23 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type GetParentLittersRequest struct {
+	Parent int64 `form:"parent" binding:"required,min=1`
+}
+
 func (server *Server) GetLittersList(ctx *gin.Context) {
+	var req GetParentLittersRequest
+	if err := ctx.BindQuery(&req); err == nil {
+		litters, err := server.store.GetListLittersByParent(ctx, req.Parent)
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+			return
+		}
+
+		ctx.JSON(http.StatusOK, litters)
+		return
+	}
+
 	litters, err := server.store.GetListAllLitters(ctx)
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
